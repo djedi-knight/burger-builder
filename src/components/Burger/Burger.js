0bb56194e57a4e8162f4f0a3f3be413a75132db4
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-09-05 15:56:31 -0400

    Implemented the Burger component

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import styles from './Burger.module.css'
+
+const burger = (props) => {
+  return (
+    <div className={styles.Burger}>
+      <BurgerIngredient type="bread-top"></BurgerIngredient>
+      <BurgerIngredient type="meat"></BurgerIngredient>
+      <BurgerIngredient type="cheese"></BurgerIngredient>
+      <BurgerIngredient type="bread-bottom"></BurgerIngredient>
+    </div>
+  );
+};
+
+export default burger;
