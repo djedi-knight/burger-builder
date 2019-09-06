97c6659a06a7116e220a80bd4d3e19b14149e883
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-09-06 09:05:33 -0400

    Reduce the ingredient array/ display message for empty ingredient list

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 import styles from './Burger.module.css'
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients).map(ingredientKey => {
+  let transformedIngredients = Object.keys(props.ingredients)
+  .map(ingredientKey => {
     return [...Array(props.ingredients[ingredientKey])].map((_, index) => {
       return <BurgerIngredient key={ingredientKey + index} type={ingredientKey}></BurgerIngredient>
     });
-  });
+  })
+  .reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={styles.Burger}>
