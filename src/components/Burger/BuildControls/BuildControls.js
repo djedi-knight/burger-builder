21ca115673fdf6268fbe8b839d64c14000ce197b
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-09-06 09:41:20 -0400

    Implemented BuildControls component

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,5 +1,21 @@
 import React from 'react';
 
-const buildControls = (props) => ();
+import BuildControl from './BuildControl/BuildControl';
+import styles from './BuildControls.module.css';
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = (props) => (
+  <div className={styles.BuildControls}>
+    {controls.map(ctrl => (
+      <BuildControl key={ctrl.type} label={ctrl.label}></BuildControl>
+    ))}
+  </div>
+);
 
 export default buildControls;
