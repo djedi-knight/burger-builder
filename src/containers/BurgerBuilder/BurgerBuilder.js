88f53bc8a8056ed3e103f8fd46b81ace7bccec84
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-09-15 23:06:51 -0400

    Connect BurgerBuilder container to the store

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
@@ -7,8 +8,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import Aux from '../../hoc/Aux/Aux';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+import * as actionTypes from '../../store/actions';
 import axios from '../../axios-orders';
-
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -19,7 +20,7 @@ const INGREDIENT_PRICES = {
 
 class BurgerBuilder extends Component {
   state = {
-    ingredients: null,
+    // ingredients: null, // Implemented as this.props.ings by Redux
     totalPrice: 4,
     purchaseable: false,
     purchasing: false,
@@ -115,7 +116,8 @@ class BurgerBuilder extends Component {
 
   render() {
     const disabledInfo = {
-      ...this.state.ingredients
+      // ...this.state.ingredients
+      ...this.props.ings
     };
     let key;
     for (key in disabledInfo) {
@@ -128,13 +130,15 @@ class BurgerBuilder extends Component {
       );
     }
     let burger = this.state.error ? <p>Ingredients cannot be loaded!</p> : <Spinner></Spinner>;
-    if (this.state.ingredients) {
+    if (this.props.ings) {
       burger = (
         <Aux>
-          <Burger ingredients={this.state.ingredients}></Burger>
+          <Burger ingredients={this.props.ings}></Burger>
           <BuildControls
-            ingredientAdded={this.addIngredientHandler}
-            ingredientRemoved={this.removeIngredientHandler}
+            // ingredientAdded={this.addIngredientHandler}
+            ingredientAdded={this.props.onIngredientAdded}
+            // ingredientRemoved={this.removeIngredientHandler}
+            ingredientRemoved={this.props.onIngredientRemoved}
             disabled={disabledInfo}
             purchaseable={this.state.purchaseable}
             ordered={this.purchaseHandler}
@@ -144,7 +148,7 @@ class BurgerBuilder extends Component {
       );
       orderSummary = (
         <OrderSummary
-          ingredients={this.state.ingredients}
+          ingredients={this.props.ings}
           price={this.state.totalPrice}
           purchaseCancelled={this.purchaseCancelHandler}
           purchaseContinued={this.purchaseContinueHandler}
@@ -162,4 +166,17 @@ class BurgerBuilder extends Component {
   }
 }
 
-export default withErrorHandler(BurgerBuilder, axios);
+const mapStateToProps = state => {
+  return {
+    ings: state.ingredients
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onIngredientAdded: (name) => dispatch({type: actionTypes.ADD_INGREDIENT, ingredientName: name}),
+    onIngredientRemoved: (name) => dispatch({type: actionTypes.REMOVE_INGREDIENT, ingredientName: name}),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(BurgerBuilder, axios));
--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -41,8 +41,8 @@ const withErrorHandler = (WrappedComponent, axios) => {
       return (
         <Aux>
           <Modal
-          show={this.state.error}
-          modalClosed={this.errorConfirmedHandler}
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
           >{this.state.error ? this.state.error.message : null}
           </Modal>
           <WrappedComponent {...this.props}></WrappedComponent>
