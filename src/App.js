ca195e1d53ffbc0fa344bc27025e823d07868b87
Shawn Daichendt <shawn.daichendt@gm.com>
2019-09-19 15:46:57 -0400

    Added the logout link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Logout from './containers/Auth/Logout/Logout';
 import Auth from  './containers/Auth/Auth';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
@@ -13,6 +14,7 @@ function App() {
       <Layout>
         <Switch>
           <Route path='/auth' component={Auth}></Route>
+          <Route path='/logout' component={Logout}></Route>
           <Route path='/checkout' component={Checkout}></Route>
           <Route path='/orders' component={Orders}></Route>
           <Route path='/' exact component={BurgerBuilder}></Route>
