1e2ee32102b30c121116f0193fdf1c9011378b66
Shawn Daichendt <shawn.daichendt@gm.com>
2019-09-20 10:34:58 -0400

    Implemented persistentauth state with localStorage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import Logout from './containers/Auth/Logout/Logout';
 import Auth from  './containers/Auth/Auth';
@@ -7,21 +8,34 @@ import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 import Layout from './hoc/Layout/Layout';
+import * as actions from './store/actions/index';
 
-function App() {
-  return (
-    <div>
-      <Layout>
-        <Switch>
-          <Route path='/auth' component={Auth}></Route>
-          <Route path='/logout' component={Logout}></Route>
-          <Route path='/checkout' component={Checkout}></Route>
-          <Route path='/orders' component={Orders}></Route>
-          <Route path='/' exact component={BurgerBuilder}></Route>
-        </Switch>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
+
+  render() {
+    return (
+      <div>
+        <Layout>
+          <Switch>
+            <Route path='/auth' component={Auth}></Route>
+            <Route path='/logout' component={Logout}></Route>
+            <Route path='/checkout' component={Checkout}></Route>
+            <Route path='/orders' component={Orders}></Route>
+            <Route path='/' exact component={BurgerBuilder}></Route>
+          </Switch>
+        </Layout>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(App);
