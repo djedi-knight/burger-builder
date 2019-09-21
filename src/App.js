75c1aaf88baee311ff88667774810f90f03fa76d
Shawn Daichendt <shawn.daichendt@gmail.com>
2019-09-21 16:24:18 -0400

    Fixed a few UI glitches

--- src/App.js
+++ src/App.js
@@ -27,6 +27,7 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
+          <Route path='/auth' component={Auth}></Route>
           <Route path='/logout' component={Logout}></Route>
           <Route path='/checkout' component={Checkout}></Route>
           <Route path='/orders' component={Orders}></Route>
